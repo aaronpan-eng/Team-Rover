2459160c9070bc84799c374da075b5427a488a00
aaronpan-eng <apan18@bu.edu>
2024-04-25 23:32:26 -0400

    edited localization node to take gps and running imu average

--- src/rover/rover/localization.py
+++ src/rover/rover/localization.py
@@ -2,49 +2,88 @@
 
 import sys
 import rclpy
+from rclpy.node import Node
 import math
 import serial
 import numpy as np
 
-# from vn.msg import Vectornav
-# from gps.msg import Customgps
+# Custom messages import
 from rover_msgs.msg import Localization, Vectornav, Customgps
 
+# Localization node that compiles gps and imu data into a single message
+class LocalizationNode(Node):
+    def __init__(self):
+        super()._init_('localization_node')
+
+        # Imu subscription and variables in node
+        self.imu_message_count = 0
+        self.yaw = 0.0
+        self.vn_subscription = self.create_subscription(
+            Vectornav,
+            'imu',
+            self.vn_callback,
+            10)
+        
+        # Gps subscription and variables in node
+        self.northing = None
+        self.easting = None
+        self.time_gps = None
+        self.gps_subscription = self.create_subscription(
+            Customgps,
+            'gps',
+            self.gps_callback,
+            10)
+        
+        # Localization publish
+        self.loc_publisher = self.create_publisher(
+            Localization,
+            'localization',
+            10
+        )
+
+    # Imu (vectornav) callback to recieve messages and average running total in 1 second
+    def vn_callback(self, msg):
+        self.yaw += msg.imu.orientation.z
+        self.imu_message_count += 1
+
+        if self.imu_message_count == 40:
+            self.yaw /= self.imu_message_count
+            self.publish_localization_message()
+            
+    # Gps callback to recieve a gps message every second
+    def gps_callback(self, msg):
+        self.northing = msg.utm_northing
+        self.easting = msg.utm_easting
+        self.time_gps = msg.header.stamp
+        self.publish_localization_message()
+
+    # Compile all the messages from gps and imu average into a single message to publish
+    def publish_localization_message(self):
+        if self.northing is not None and self.easting is not None and self.time_gps is not None and self.imu_message_count == 40:
+            # Collect messages from gps and imu callback to compile here
+            localization_msg = Localization()
+            localization_msg.time_gps = self.time_gps
+            localization_msg.yaw = self.yaw
+            localization_msg.northing = self.northing
+            localization_msg.eating = self.easting
+            self.loc_publisher(localization_msg)
+
+            # Reset yaw running total and total imu message count to 0
+            self.imu_message_count = 0
+            self.yaw = 0.0
+
+# Main loop
+def main():
+    # Initialize and run node
+    rclpy.init()
+    node = LocalizationNode()
+    rclpy.spin(node)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    node.destroy_node()
+    rclpy.shutdown()
+
 if __name__ == '__main__':
-    # Initialize node, message, and publisher for localization
-    rclpy.init_node('localization')
-    localization = Localization()
-    localization_pub = rclpy.Publisher('/localization', Localization, queue_size = 5)
-
-    # Initialize subscribers from imu and gps
-    imu = rclpy.Subscriber('/imu', Vectornav, queue_size=5)
-    gps = rclpy.Subscriber('/gps', Customgps, queue_size=5)
-
-    while not rclpy.is_shutdown():
-        # Extracting messages
-        northing = gps.utm_northing
-        easting = gps.utm_easting
-
-        yaw = imu.imu.orientation.z
-        mag_x = imu.mag_field.magnetic_field.x
-        mag_y = imu.mag_field.magnetic_field.y
-
-        time_gps = gps.header.stamp
-        time_gps = time_imu.to_sec()
-
-        time_imu = imu.stamp
-        time_imu = time_imu.to_sec()
-
-        # TODO can add calibration or other calculations here
-
-        # Setting messages to publish
-        localization.time_imu = time_imu
-        localization.time_gps = time_gps
-        localization.northing = northing
-        localization.easting = easting
-        localization.yaw = yaw
-        localization.mag_x = mag_x
-        localization.mag_y = mag_y
-
-        # Publishing
-        localization_pub.publish(localization)
+    main()
