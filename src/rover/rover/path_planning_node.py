1044764e50cf3988f0e0bc16c8c6afd4ccdf1360
aaronpan-eng <apan18@bu.edu>
2024-04-26 13:26:30 -0400

    removed averaging process in path planning b/c already done in localization node

--- src/rover/rover/path_planning_node.py
+++ src/rover/rover/path_planning_node.py
@@ -11,10 +11,10 @@ class PathPlanningNode(Node):
         super().__init__('path_planning_node')
 
         # Initialize variables for averaging GPS and IMU data
-        self.avg_northing = 0.0
-        self.avg_easting = 0.0
-        self.avg_yaw = 0.0
-        self.count = 0
+        self.northing = 0.0
+        self.easting = 0.0
+        self.yaw = 0.0
+        # ?self.count = 0
 
         self.gps_flag = 0
         # TODO: set flag for gps coordinates to default to something later on
@@ -49,62 +49,29 @@ class PathPlanningNode(Node):
             self.goal_easting = msg.easting - 87
 
         # Average GPS northing, easting, and IMU yaw over 5 seconds
-        self.avg_northing += msg.northing
-        self.avg_easting += msg.easting
-        self.avg_yaw += msg.yaw
-        self.count += 1
+        self.northing = msg.northing
+        self.easting = msg.easting
+        self.yaw = msg.yaw
 
-        if self.count == 5:  # 5 seconds have passed
-            self.avg_northing /= 5.0
-            self.avg_easting /= 5.0
-            self.avg_yaw /= 5.0
-
-            # TODO: if self.goal flag is off, then set the GPS to a offset from current locatoin
-
-            # calculate angle to rotate
-            phi = atan2(self.goal_easting - self.avg_easting, self.goal_northing - self.avg_northing)*(180/pi)
-            if abs(self.avg_yaw) > (180-abs(phi)):
-                if phi > 0:
-                    goal_angle = (phi - self.avg_yaw) - 360
-                elif phi < 0:
-                    goal_angle = (phi - self.avg_yaw) + 360
-            else:
-                goal_angle = phi - self.avg_yaw
-
-
-            # # Calculate distance to travel and angle to rotate
-            # distance_to_travel = sqrt(
-            #     (self.goal_northing - self.avg_northing)**2 + (self.goal_easting - self.avg_easting)**2)
-            # deg_to_rot = degrees(atan2(
-            #     self.goal_northing - self.avg_northing, self.goal_easting - self.avg_easting))
-
-            # Calculate distance to travel
-            distance_to_travel = sqrt(
-                (self.goal_northing - self.avg_northing) ** 2 + (self.goal_easting - self.avg_easting) ** 2)
-            
-            # COMMENTED OUT #####
-            # goal_angle = atan2(
-            #     self.goal_northing - self.avg_northing, self.goal_easting - self.avg_easting)
-            # # Ensure the angle is between 0 and 360 degrees
-            # goal_angle = (goal_angle + 2 * pi) % (2 * pi)
-            # # Calculate angle to rotate relative to current yaw angle from IMU
-            # deg_to_rot = degrees(goal_angle - self.avg_yaw)
-            # # # Ensure the angle is between -180 and 180 degrees
-            # # deg_to_rot = (deg_to_rot + 180) % 360 - 180
-            # COMMENTED OUT #####
-
-
-            # Publish path planning data
-            path_planning_msg = PathPlanning()
-            path_planning_msg.distance_to_travel = distance_to_travel
-            path_planning_msg.deg_to_rot = goal_angle # [-180,180], + being turn right, - being turn left
-            self.path_planning_pub.publish(path_planning_msg)
-
-            # Reset variables for next averaging period
-            self.avg_northing = 0.0
-            self.avg_easting = 0.0
-            self.avg_yaw = 0.0
-            self.count = 0
+        # calculate angle to rotate
+        phi = atan2(self.goal_easting - self.easting, self.goal_northing - self.northing)*(180/pi)
+        if abs(self.yaw) > (180-abs(phi)):
+            if phi > 0:
+                goal_angle = (phi - self.yaw) - 360
+            elif phi < 0:
+                goal_angle = (phi - self.yaw) + 360
+        else:
+            goal_angle = phi - self.yaw
+        
+        # Calculate distance to travel
+        distance_to_travel = sqrt(
+                (self.goal_northing - self.northing) ** 2 + (self.goal_easting - self.easting) ** 2)
+        
+        # Publish message
+        path_planning_msg = PathPlanning()
+        path_planning_msg.distance_to_travel = distance_to_travel
+        path_planning_msg.deg_to_rot = goal_angle # [-180,180], + being turn right, - being turn left
+        self.path_planning_pub.publish(path_planning_msg)
 
     def obstacles_callback(self, msg):
         # Process obstacles data if needed
