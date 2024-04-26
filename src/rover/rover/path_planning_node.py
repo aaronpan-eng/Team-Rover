7789781e933513f220b5fd13aa7a1c7cc1ca98db
aaronpan-eng <apan18@bu.edu>
2024-04-26 13:08:22 -0400

    changed localization angle calc, updated control node to work with motors

--- src/rover/rover/path_planning_node.py
+++ src/rover/rover/path_planning_node.py
@@ -15,6 +15,8 @@ class PathPlanningNode(Node):
         self.avg_easting = 0.0
         self.avg_yaw = 0.0
         self.count = 0
+
+        self.gps_flag = 0
         # TODO: set flag for gps coordinates to default to something later on
 
         # Subscribe to localization, obstacles, and LoRa topics
@@ -42,6 +44,10 @@ class PathPlanningNode(Node):
             PathPlanning, 'path_planning_topic', 10)
 
     def localization_callback(self, msg):
+        if self.gps_flag == 0:
+            self.goal_northing = msg.northing - 138
+            self.goal_easting = msg.easting - 87
+
         # Average GPS northing, easting, and IMU yaw over 5 seconds
         self.avg_northing += msg.northing
         self.avg_easting += msg.easting
@@ -55,8 +61,7 @@ class PathPlanningNode(Node):
 
             # TODO: if self.goal flag is off, then set the GPS to a offset from current locatoin
 
-            #calculate distance
-            # TODO: substitute this angle calculation in
+            # calculate angle to rotate
             phi = atan2(self.goal_easting - self.avg_easting, self.goal_northing - self.avg_northing)*(180/pi)
             if abs(self.avg_yaw) > (180-abs(phi)):
                 if phi > 0:
@@ -70,10 +75,10 @@ class PathPlanningNode(Node):
             # # Calculate distance to travel and angle to rotate
             # distance_to_travel = sqrt(
             #     (self.goal_northing - self.avg_northing)**2 + (self.goal_easting - self.avg_easting)**2)
-            # deg_clockwise_to_rot = degrees(atan2(
+            # deg_to_rot = degrees(atan2(
             #     self.goal_northing - self.avg_northing, self.goal_easting - self.avg_easting))
 
-            # Calculate distance to travel and angle to rotate
+            # Calculate distance to travel
             distance_to_travel = sqrt(
                 (self.goal_northing - self.avg_northing) ** 2 + (self.goal_easting - self.avg_easting) ** 2)
             
@@ -83,16 +88,16 @@ class PathPlanningNode(Node):
             # # Ensure the angle is between 0 and 360 degrees
             # goal_angle = (goal_angle + 2 * pi) % (2 * pi)
             # # Calculate angle to rotate relative to current yaw angle from IMU
-            # deg_clockwise_to_rot = degrees(goal_angle - self.avg_yaw)
+            # deg_to_rot = degrees(goal_angle - self.avg_yaw)
             # # # Ensure the angle is between -180 and 180 degrees
-            # # deg_clockwise_to_rot = (deg_clockwise_to_rot + 180) % 360 - 180
+            # # deg_to_rot = (deg_to_rot + 180) % 360 - 180
             # COMMENTED OUT #####
 
 
             # Publish path planning data
             path_planning_msg = PathPlanning()
             path_planning_msg.distance_to_travel = distance_to_travel
-            path_planning_msg.deg_clockwise_to_rot = goal_angle # TODO: change message to now describe it as [-180,180], + being turn right, - being turn left
+            path_planning_msg.deg_to_rot = goal_angle # [-180,180], + being turn right, - being turn left
             self.path_planning_pub.publish(path_planning_msg)
 
             # Reset variables for next averaging period
@@ -109,6 +114,7 @@ class PathPlanningNode(Node):
         # Extract goal northing and easting from LoRa message
         self.goal_northing = msg.northing
         self.goal_easting = msg.easting
+        self.gps_flag = 1
 
 
 def main(args=None):
